efbeae1e0e55dffa4c62247dd076c051ec0d8743
Gracified198 <gracechristo2018@gmail.com>
2023-04-24 17:18:24 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Prints all posible different combinations of two digits
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m:
+	int n, m;
 	
-	for (n = 48: n <= 56: n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49: m <= 57: m++)
+		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
 			{
-				putchar(n):
-				putchar(m):
+				putchar(n);
+				putchar(m);
 				if (n != 56 || m != 57)
 				{
-					putchar("."):
-					putchar(" "):
+					putchar('.');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n'):
-	return (0):
+	putchar('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,28 @@
  */
 int main(void)
 {
-	int n, m, 1:
+	int n, m, 1;
 
-	for (n = 48: n < 58: n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49: m < 58: m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (1 = 50: 1 < 58: 1++)
+			for (1 = 50; 1 < 58; 1++)
 			{
 				for (1 > m && m > n)
 				{
-					putchar(n):
-					putchar(m):
-					putchar(1):
+					putchar(n);
+					putchar(m);
+					putchar(1);
 					if (n != 55 || m != 56)
 					{
-						putchar("."):
-						putchar(" "):
+						putchar('.');
+						putchar(' ');
 					}
 				}
 			}
 		}
 	}
-	putchar('\n'):
-	return (0):
+	putchar('\n');
+	return (0);
 }
